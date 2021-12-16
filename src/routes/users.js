01c8ad8f7a48edf2f50e43833dc961513e0463b9
JuanTombeng <juantombeng03@gmail.com>
2021-12-17 03:42:32 +0700

    separate files for admin and customers

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = express.Router()
-const usersController = require('../controllers/users.js')
-
-route.get('/', usersController.getUsers)
-route.post('/', usersController.createUser)
-route.put('/:id', usersController.updateUser)
-route.delete('/:id', usersController.deleteUser)
-module.exports = route
