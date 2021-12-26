dcadd65556683d612c656acbb1f07dd08211468e
JuanTombeng <juantombeng03@gmail.com>
2021-12-27 02:37:37 +0700

    additional fields

--- zwallet.sql
+++ zwallet.sql
@@ -1,15 +1,20 @@
-CREATE DATABASE zwallet;
+CREATE DATABASE zwallet_app;
+
+USE DATABASE zwallet_app;
 
 CREATE TABLE users (id VARCHAR(64) NOT NULL PRIMARY KEY, 
     username VARCHAR(50) NOT NULL, 
     email VARCHAR(50) NOT NULL, 
-    password VARCHAR(100) NOT NULL, 
+    password VARCHAR(100) NOT NULL,
+    pin INT(6) NOT NULL, 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL);
 
 CREATE TABLE profiles (id_user VARCHAR(64) NOT NULL, 
     first_name VARCHAR(50) NOT NULL, 
     last_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    profile_picture BLOB NULL,
     FOREIGN KEY (id_user) REFERENCES users(id)
     ON DELETE CASCADE);
 
@@ -26,12 +31,17 @@ CREATE TABLE transactions (id VARCHAR(64) NOT NULL PRIMARY KEY,
     from_account_id VARCHAR(64) NOT NULL, 
     to_account_id VARCHAR(64) NOT NULL,
     amount INT(10) NOT NULL DEFAULT 0, 
+    transaction_type VARCHAR(50) NOT NULL,
+    notes TEXT NULL,
+    status TINYINT(1) NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL,
     FOREIGN KEY (from_account_id) REFERENCES accounts(id)
-    ON DELETE CASCADE, 
+    ON DELETE RESTRICT, 
     FOREIGN KEY (to_account_id) REFERENCES accounts(id)
-    ON DELETE CASCADE);
+    ON DELETE RESTRICT);
+
+-- CREATE TABLE transaction_details (id VARCHAR(64) NOT NULL PRIMARY KEY)
 
 
 -- SELECT accounts.id, accounts.id_user, users.username, accounts.account_number, contact_holder.id_user_holder 
