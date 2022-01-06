17d4299317e890670adc2c03adf7a803564e1b7e
JuanTombeng <juantombeng03@gmail.com>
2022-01-07 00:58:39 +0700

    adding router get user by id

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,10 +4,10 @@ const usersController = require('../controllers/users.js')
 const validator = require('../middleware/common')
 
 route.get('/', usersController.getUsers)
+route.get('/:id', usersController.getUserById)
 // route.get('/:id', usersController.userDetails)
 route.post('/signup', validator.userInputValidation, usersController.signup)
 route.post('/login', usersController.login)
-route.get('/:id', usersController.getUserById)
 route.put('/update/:id', validator.userInputValidation, usersController.updateUser)
 route.delete('/delete/:id', usersController.deleteUser)
 
