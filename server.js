04866cfb6188dce7b50f2cfe26844e56a94c436a
Juan Tombeng <juantombeng03@gmail.com>
2022-01-28 00:59:55 +0700

    create v2 route

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 4000;
 const commonHelper = require('./src/v1/helper/common')
-const version1 = require('./src/v1/routes')
 const cors = require('cors')
+
+const version1 = require('./src/v1/routes')
+const version2 = require('./src/v2/routes')
 
 app.use(express.json());
 app.use(morgan('dev'))
 app.use(cors())
 app.use('/v1', version1)
+app.use('/v2', version2)
 
 // URL not Found handler
 app.use(commonHelper.handleURLNotFound)
