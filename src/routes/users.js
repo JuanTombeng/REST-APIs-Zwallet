01f06ec6950e10bc9d9d1b72e7b405231cfdb03e
Juan Tombeng <juantombeng03@gmail.com>
2022-01-27 22:03:46 +0700

    adding user verification to all endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,12 +3,12 @@ const route = express.Router()
 const usersController = require('../controllers/users.js')
 const validator = require('../middleware/common')
 
-route.get('/', usersController.getUsers)
-route.get('/:id', usersController.getUserDetails)
+route.get('/', validator.userVerification, usersController.getUsers)
+route.get('/:id', validator.userVerification, usersController.getUserDetails)
 route.post('/signup', validator.userInputValidation, usersController.signup)
 route.post('/login', usersController.login)
-route.put('/update/:id', validator.userInputValidation, usersController.updateUser)
-route.delete('/delete/:id', usersController.deleteUser)
+route.put('/update/:id', validator.userVerification, validator.userInputValidation, usersController.updateUser)
+route.delete('/delete/:id', validator.userVerification, usersController.deleteUser)
 route.get('/email-verification/:id', validator.emailTokenVerification)
 
 module.exports = route
