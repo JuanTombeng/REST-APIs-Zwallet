043ea8b22c7dca472f6036042f88ce325bb1ccb9
Juan Tombeng <juantombeng03@gmail.com>
2022-01-27 22:05:01 +0700

    token verify for user verification

--- src/middleware/common.js
+++ src/middleware/common.js
@@ -53,6 +53,32 @@ const transactionInputValidation = (req, res, next) => {
     }
 }
 
+const userVerification = async (req, res, next) => {
+    try {
+        let token
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer') ) {
+            token = req.headers.authorization.split(' ')[1]
+        } else {
+            return next(createError(403, 'Server Need Token'))
+        }
+        const verifyOptions = {
+            issuer : `zwallet`
+        }
+        const secretKey = process.env.SECRET_KEY
+        const decoded = jwt.verify(token, secretKey, verifyOptions)
+        req.email = decoded.email
+        next()
+    } catch (error) {
+        if (error && error.name === `JsonWebTokenError`) {
+            return next(createError(400, 'Token Invalid'))
+        } else if (error && error.name === 'TokenExpiredError') {
+            return next(createError(400, 'Token Expired'))
+        } else {
+            return next(createError(400, 'Token not activated'))
+        }
+    }
+}
+
 const emailTokenVerification = async (req, res, next) => {
     try {
         const receivedToken = req.params.id
@@ -76,5 +102,6 @@ const emailTokenVerification = async (req, res, next) => {
 module.exports = {
     userInputValidation,
     transactionInputValidation,
+    userVerification,
     emailTokenVerification
 }
