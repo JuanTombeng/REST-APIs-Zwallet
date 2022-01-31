80ac1cd790ea763fc200b9deb353b68bcdd5420b
Juan Tombeng <juantombeng03@gmail.com>
2022-02-01 00:38:34 +0700

    resetPasswordEmailTokenVerification

--- src/v2/middleware/authentication.js
+++ src/v2/middleware/authentication.js
@@ -55,17 +55,27 @@ const emailTokenVerification = async (req, res, next) => {
 
 const resetPasswordEmailTokenVerification = async (req, res, next) => {
     try {
-        const emailToken = req.params.token
-        const secretKey = process.env.SECRET_KEY
+        let token
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1]
+        } else {
+            return next(createError(403, 'Server Need Token'))
+        }
         const verifyOptions = {
             issuer : 'zwallet'
         }
-        const decoded = jwt.verify(emailToken, secretKey, verifyOptions)
+        const secretKey = process.env.SECRET_KEY
+        const decoded = jwt.verify(token, secretKey, verifyOptions)
         req.decoded = decoded
         next()
-        res.redirect('http://localhost:3000/reset-password-form')
     } catch (error) {
-        
+        if (error && error.name === `JsonWebTokenError`) {
+            return next(createError(400, 'Token Invalid'))
+        } else if (error && error.name === 'TokenExpiredError') {
+            return next(createError(400, 'Token Expired'))
+        } else {
+            return next(createError(400, 'Token not activated'))
+        }
     }
 }
 
