bd88b4f2cdb92438a70ba12668b4ed2b16c51061
Juan Tombeng <juantombeng03@gmail.com>
2022-02-02 03:13:34 +0700

    isAdmin authenticator

--- src/v2/middleware/authentication.js
+++ src/v2/middleware/authentication.js
@@ -3,6 +3,37 @@ const jwt = require('jsonwebtoken')
 const userQuery = require('../models/users')
 const commonHelper = require('../helper/common')
 
+const isAdmin = async (req, res, next) => {
+    try {
+        let token
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1]
+        } else {
+            return next(createError(403, 'Server Need Token'))
+        }
+        const verifyOptions = {
+            issuer : 'zwallet'
+        }
+        const secretKey = process.env.SECRET_KEY
+        const decoded = jwt.verify(token, secretKey, verifyOptions)
+        if (decoded.role === 'admin') {
+            res.redirect('http://localhost:3000/login')
+            return next(createError(400, 'You are not authorized to continue'))
+        } else {
+            req.decoded = decoded
+            next()
+        }
+    } catch (error) {
+        if (error && error.name === `JsonWebTokenError`) {
+            return next(createError(400, 'Token Invalid'))
+        } else if (error && error.name === 'TokenExpiredError') {
+            return next(createError(400, 'Token Expired'))
+        } else {
+            return next(createError(400, 'Token not activated'))
+        }
+    }
+}
+
 const userTokenVerification = async (req, res, next) => {
     try {
         let token
@@ -80,6 +111,7 @@ const resetPasswordEmailTokenVerification = async (req, res, next) => {
 }
 
 module.exports = {
+    isAdmin,
     userTokenVerification,
     emailTokenVerification,
     resetPasswordEmailTokenVerification
