f95853485fe13d0b1ceeb11a75815542b8068971
Juan Tombeng <juantombeng03@gmail.com>
2022-02-09 19:56:27 +0700

    adding redis

--- src/v2/middleware/redis.js
+++ src/v2/middleware/redis.js
@@ -1,34 +1,48 @@
-// const client = require('../config/redis')
-// const commonHelper = require('../helper/common')
+const client = require('../config/redis')
+const commonHelper = require('../helper/common')
 
-// const hitCacheUserEmail = async (req, res, next) => {
-//     const {email, role, active} = req.decoded
-//     const user = await client.get(`user/:${email}`)
-//     if (user !== null) {
-//         commonHelper.response(res, JSON.parse(user), 200, `User ${email} fetched from Redis Server`)
-//     } else {
-//         next()
-//     }
-// }
+const hitCacheUserEmail = async (req, res, next) => {
+    const {email, role, active} = req.decoded
+    const user = await client.get(`user/:${email}`)
+    if (user !== null) {
+        commonHelper.response(res, JSON.parse(user), 200, `User ${email} fetched from Redis Server`)
+    } else {
+        next()
+    }
+}
 
-// const hitCacheUserListByList = async (req, res, next) => {
-//     const {email, role, active} = req.decoded
-//     const contactList = await client.get(`contact-list/:${email}`)
-//     if (contactList !== null) {
-//         commonHelper.response(res, JSON.parse(contactList), 200, `User ${email} Contact List is fetched from Redis Server`)
-//     } else {
-//         next()
-//     }
-// }
+const hitCacheUserListByList = async (req, res, next) => {
+    const {email, role, active} = req.decoded
+    const contactList = await client.get(`contact-list/${email}`)
+    if (contactList !== null) {
+        commonHelper.response(res, JSON.parse(contactList), 200, `User ${email} Contact List is fetched from Redis Server`)
+    } else {
+        next()
+    }
+}
 
-// const clearRedisUser = (req, res, next) => {
-//     client.del('user')
-//     client.del('contact-list')
-//     next()
-// }
+const clearRedisUser = (req, res, next) => {
+    const {email, role, active} = req.decoded
+    client.del(`user/${email}`)
+    next()
+}
 
-// module.exports = {
-//     hitCacheUserEmail,
-//     hitCacheUserListByList,
-//     clearRedisUser
-// }
+const clearRedisContactList = (req, res, next) => {
+    const {email, role, active} = req.decoded
+    client.del(`contact-list/${email}`)
+    next()
+}
+
+const clearRedisContactMemberDetail = (req, res, next) => {
+    const userTargetID = req.params.id
+    client.del(`contact-list/${userTargetID}`)
+    next()
+}
+
+module.exports = {
+    hitCacheUserEmail,
+    hitCacheUserListByList,
+    clearRedisUser,
+    clearRedisContactList,
+    clearRedisContactMemberDetail
+}
