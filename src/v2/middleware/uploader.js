68544b3e3b24b0ff13ae00a80ba65e53175bca1c
Juan Tombeng <juantombeng03@gmail.com>
2022-03-29 11:03:36 +0700

    testing multer

--- src/v2/middleware/uploader.js
+++ src/v2/middleware/uploader.js
@@ -1,20 +1,48 @@
-const multer = require('multer')
+// const multer = require('multer')
 
-const maxSize = 1024 * 1024
+// const maxSize = 1024 * 1024
+// const storage = multer.diskStorage({
+//     destination : (req, file, cb) => {
+//         cb(null, './src/uploads')
+//     },
+//     filename : (req, file, cb) => {
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//         if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+//             cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
+//         } else {
+//             cb(null, false);
+//             return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+//         }
+//     }
+// })
+
+// const upload = multer({storage : storage}, {fileSize : maxSize})
+// module.exports = {upload}
+
+const multer = require("multer");
+
 const storage = multer.diskStorage({
-    destination : (req, file, cb) => {
-        cb(null, './src/uploads')
-    },
-    filename : (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
-        } else {
-            cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-        }
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+  }
+});
 
-const upload = multer({storage : storage}, {fileSize : maxSize})
-module.exports = {upload}
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: fileFilter
+});
+
+module.exports = upload;
